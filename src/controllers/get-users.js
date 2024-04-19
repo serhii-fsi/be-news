1cd8a878af98637d73edbc7009291de0f2b6e552
serhii-fsi <97468007+serhii-fsi@users.noreply.github.com>
2024-04-19 16:22:54 +0100

    Task 10 - get users - tests, implementation, endpoint description

---
+++ src/controllers/get-users.js
@@ -0,0 +1,14 @@
+const fetchUsers = require('../models/fetch-users');
+
+
+const getUsers = async (req, res, next) => {
+    try {
+        const users = await fetchUsers();
+        res.status(200).send({ users });
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+module.exports = getUsers;
---
+++ src/models/fetch-users.js
@@ -0,0 +1,23 @@
+const db = require('../db/connection');
+const ModelError = require('./errors/model-error');
+
+
+const fetchUsers = async () => {
+    try {
+        const { rows } = await db.query(
+            `SELECT
+                username,
+                name,
+                avatar_url
+            FROM
+                users;`,
+        );
+        return rows;
+    } catch (error) {
+        const modelErr = new ModelError({ psql: error, msg: 'PSQL Error' });
+        throw modelErr.toAppError();
+    }
+};
+
+
+module.exports = fetchUsers;
